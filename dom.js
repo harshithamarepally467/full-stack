a883b4fb4841a9c3f6e89369fdf0075194e49e65
harshithamarepally467 <harshithamarepally@gmail.com>
2025-06-25 15:19:24 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,13 @@
+document.title= "Dom in  javascript";
+document.getElementById("MRCET").innerHTML = "Hello World";
+document.getElementById("mr").style.color="red";
+document.getElementById("mr").style.fontSize="30px";
+document.getElementById("MRCET").style.backgroundColor="yellow";
+document.getElementById("ds1").innerHTML = "Java Script";
+document.getElementById("ds1").style.color="blue";
+document.body.style.background = "lightgreen";
+document.getElementsByClassName("MRCET")[0].style.color="red";
+
+document.getElementsByClassName("ds")[0].style.color="red";
+
+
